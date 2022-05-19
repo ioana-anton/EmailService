4a38c32d5e2bb50aa37c843f16612e21efa4f396
ioana-anton <94392890+ioana-anton@users.noreply.github.com>
2022-05-19 10:49:32 +0300

    Updated Email Template

--- src/main/java/com/example/emailservice/RegisterQueueConsumer.java
+++ src/main/java/com/example/emailservice/RegisterQueueConsumer.java
@@ -5,13 +5,13 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
-@Configuration
+//@Component
+//@Configuration
 public class RegisterQueueConsumer {
-       // @RabbitListener(queues = "${javainuse.rabbitmq.queue}")
-       @RabbitListener(queues = "register-orders")
-        public void recievedMessage(RegisterEmailDTO msg) {
-            System.out.println("Received Message From RabbitMQ: " + msg.getEmail());
-        }
+//       // @RabbitListener(queues = "${javainuse.rabbitmq.queue}")
+//       @RabbitListener(queues = "register-orders")
+//        public void recievedMessage(RegisterEmailDTO msg) {
+//            System.out.println("Received Message From RabbitMQ: " + msg.getEmail());
+//        }
 
 }
